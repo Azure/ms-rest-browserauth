3bacc756f3614d209658910fff573389afd27499
Rikki Gibson <rigibson@microsoft.com>
2018-09-21 12:14:03 -0700

    Fix package.json main update in scripts

--- .scripts/local.ts
+++ .scripts/local.ts
@@ -1,5 +1,5 @@
 import * as dependencies from "./dependencies";
 import * as repository from "./repository";
 
-dependencies.updatePackageJsonMain(dependencies.getThisRepositoryFolderPath(), "./lib/msRest.ts");
+dependencies.updatePackageJsonMain(dependencies.getThisRepositoryFolderPath(), "./lib/index.ts");
 dependencies.updateLocalDependencies(repository.packageFolders, "local", dependencies.getLocalDependencyVersion);
--- .scripts/preview.ts
+++ .scripts/preview.ts
@@ -1,5 +1,5 @@
 import * as dependencies from "./dependencies";
 import * as repository from "./repository";
 
-dependencies.updatePackageJsonMain(dependencies.getThisRepositoryFolderPath(), "./dist/lib/msRest.js");
+dependencies.updatePackageJsonMain(dependencies.getThisRepositoryFolderPath(), "./dist/lib/index.js");
 dependencies.updateLocalDependencies(repository.packageFolders, "preview", dependencies.getPreviewDependencyVersion);
