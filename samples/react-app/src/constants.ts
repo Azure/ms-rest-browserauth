305002dc4934f7e58e275ecbddb7c2f7c1e25e9b
Maor Leger <malege@microsoft.com>
2021-02-08 16:32:53 -0800

    working implementation

--- samples/react-app/src/constants.ts
+++ samples/react-app/src/constants.ts
@@ -1,3 +1,15 @@
-export const clientId = "<client id for your Azure AD app>";
-export const tenant = "<optional tenant for your organization>";
-export const subscriptionId = "<id of the Azure subscription to use>";
+function getEnvironmentVariable(name: string) {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(
+      `Missing environment variable ${name}. Please make sure that you copy sample.env as just ".env" and populate the necessary values.`
+    );
+  }
+  return value;
+}
+
+export const clientId = getEnvironmentVariable("REACT_APP_CLIENT_ID");
+export const tenant = getEnvironmentVariable("REACT_APP_TENANT_ID");
+export const subscriptionId = getEnvironmentVariable(
+  "REACT_APP_SUBSCRIPTION_ID"
+);
