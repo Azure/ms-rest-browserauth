59884c86e25474ad1355946661c2418c970da93d
Maor Leger <malege@microsoft.com>
2021-02-11 08:07:56 -0800

    add explanation of how env files are read

--- samples/react-app/src/constants.ts
+++ samples/react-app/src/constants.ts
@@ -1,3 +1,8 @@
+// react-scripts supports reading custom environment variables from an .env file
+// and will automatically import any environment variable defined there if it
+// starts with the pattern `REACT_APP_`
+// Please refer to this document for more information:
+// https://create-react-app.dev/docs/adding-custom-environment-variables
 function getEnvironmentVariable(name: string) {
   const value = process.env[name];
   if (!value) {
